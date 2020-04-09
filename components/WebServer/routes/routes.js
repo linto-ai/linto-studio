ba38e2723c57936eb7d7a8711423506c79caea47
rlopezdev <rlopez@linagora.com>
2020-04-09 15:37:16 +0200

    fix url login

--- components/WebServer/routes/routes.js
+++ components/WebServer/routes/routes.js
@@ -2,7 +2,7 @@ const debug = require('debug')('app:webserver:routes')
 
 module.exports = (webServer) => {
     return {
-        "/login": require('./api/login')(webServer),
+        "/login": require('./login')(webServer),
         "/api/users": require('./api/users')(webServer),
         "/api/user": require('./api/user')(webServer)
     }
