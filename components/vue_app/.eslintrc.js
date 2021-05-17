12b18f2359e7fa11b4b1c6c9e2a5de5a5207eec4
rlopezdev <rlopez@linagora.com>
2021-05-17 09:51:20 +0200

    update version of vue-cli

--- components/vue_app/.eslintrc.js
+++ components/vue_app/.eslintrc.js
@@ -1,3 +1,15 @@
 module.exports = {
-    // no eslint
+    /*
+        root: true,
+        env: {
+            node: true,
+        },
+        extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
+        parserOptions: {
+            parser: "babel-eslint",
+        },
+        rules: {
+            "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+            "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+        },*/
 }
