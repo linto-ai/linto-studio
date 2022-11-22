194875491a1fa03e96f3e4def957cd29c03eafee
HOUPERT <yoann.houpert@yahoo.fr>
2022-11-22 17:30:15 +0100

    added an service exception

--- components/WebServer/controllers/services/utility.js
+++ components/WebServer/controllers/services/utility.js
@@ -1,18 +1,24 @@
 const debug = require('debug')('linto:conversation-manager:components:WebServer:controller:services:utility')
-const { ConversationError } = require(`${process.cwd()}/components/WebServer/error/exception/conversation`)
+const { ConversationError } = require(`${process.cwd()}/components/WebServer/error/exception/service`)
 
 const axios = require(`${process.cwd()}/lib/utility/axios`)
 
 async function listSaasServices() {
-  const gateway_services = process.env.GATEWAY_SERVICES
-  let services = []
+  try {
 
-  const saas_service_info = await axios.get(gateway_services + '/gateway/services')
-  for (const transcription_service of saas_service_info.transcription) {
-    services.push(transcription_service)
+    const gateway_services = process.env.GATEWAY_SERVICES
+    let services = []
+
+    const saas_service_info = await axios.get(gateway_services + '/gateway/services')
+    for (const transcription_service of saas_service_info.transcription) {
+      services.push(transcription_service)
+    }
+
+    if (services.length === 0) throw new ServiceError('No transcription service found')
+    return services
+  } catch (err) {
+    throw new ServiceError('Error while listing services')
   }
-
-  return services
 }
 
 
