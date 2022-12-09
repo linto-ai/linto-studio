a92e96cead5a9374a6bbfcce796e72ac59eafcac
HOUPERT <yoann.houpert@yahoo.fr>
2022-12-09 11:15:38 +0100

    Added healtcheck to swagger

--- components/WebServer/apidoc/index.js
+++ components/WebServer/apidoc/index.js
@@ -1,8 +1,10 @@
 module.exports = {
   ...require('./api/auth.json'),
+  ...require('./api/healthcheck.json'),
   ...require('./api/services.json'),
   ...require('./api/users.json')
 
   // ...require('./api/conversations.json'),
   // ...require('./api/organizations.json'),
+  // ...require('./api/test.json')
 }
