9ec1cadfa1d9e56a91b7ad368111915782a7a6f5
HOUPERT <yoann.houpert@yahoo.fr>
2023-01-16 15:26:52 +0100

    update jobs status on transcription ending

--- components/WebServer/controllers/jobsHandler.js
+++ components/WebServer/controllers/jobsHandler.js
@@ -54,8 +54,7 @@ async function createJobInterval(host, conversation, processing_job) {
             }
 
             if (job_info.state === 'done' && job_info.result_id) { //triger last request
-                updateConversation(conversation, processing_job, job_info)
-
+                conversation.jobs.transcription = job_info
                 getResult(host, processing_job, job_status, conversation)
 
                 debug('jobs done')
@@ -87,7 +86,6 @@ function updateConversation(conversation, processing_job, job_info) {
         conversation.jobs.keyword = job_info
     }
     ConvoModel.updateJob(id, conversation.jobs)
-
 }
 
 module.exports = { createJobInterval }
