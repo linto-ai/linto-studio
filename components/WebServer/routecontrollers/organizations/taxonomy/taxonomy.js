0f1697d252a7c7ddf098f5504860c61b116bcddf
HOUPERT <yoann.houpert@yahoo.fr>
2023-03-21 11:28:49 +0100

    update search by tagId and catId

--- components/WebServer/routecontrollers/organizations/taxonomy/taxonomy.js
+++ components/WebServer/routecontrollers/organizations/taxonomy/taxonomy.js
@@ -1,3 +1,8 @@
+const { resolve4 } = require('dns')
+const { accessSync } = require('fs')
+const nodemon = require('nodemon')
+const { resolve } = require('path/win32')
+
 const debug = require('debug')('linto:conversation-manager:components:WebServer:routecontrollers:taxonomy:taxonomy')
 const model = require(`${process.cwd()}/lib/mongodb/models`)
 
@@ -6,24 +11,62 @@ const organizationUtility = require(`${process.cwd()}/components/WebServer/contr
 const {
   OrganizationError
 } = require(`${process.cwd()}/components/WebServer/error/exception/organization`)
-
 async function searchTaxonomy(req, res, next) {
   try {
-    let conversation = []
-    let convAccess = (await organizationUtility
+    // Check if tags are provided
+
+    if (!req.body.tags) throw new OrganizationError('Tags are required')
+
+    const userConversationsIds = (await organizationUtility
       .getUserConversationFromOrganization(req.payload.data.userId, req.params.organizationId))
       .map(conv => conv._id)
 
-      debug(convAccess)
+    // Search for conversations based on tags and access
+    const conversations = await model.search.conversation.getByIdsAndTag(userConversationsIds, req.body.tags.split(','))
+    const tagIds = conversations.flatMap(conv => conv.tags)
+    const tagCount = tagIds.reduce(function (prev, cur) {
+      prev[cur] = (prev[cur] || 0) + 1
+      return prev
+    }, {})
 
-    if (!req.body.tags) throw new OrganizationError('Tags are required')
-    let convTag = (await model.search.conversation.getByIdsAndTag(convAccess, req.body.tags.split(','))).map(conv => conv.tags)
+    const uniqueTagIds = [...new Set([...req.body.tags.split(','), ...tagIds])] // add body tag at the start of the list
+    // Generate category list based on tags
+    let categories = {}
 
-    debug(convTag)
+    for (const tagId of uniqueTagIds) {
+      const tag = {
+        ...(await model.tag.getById(tagId))[0],
+        count: tagCount[tagId]
+      }
+      const categoryId = tag.categoryId
 
-    res.status(200).send(conversation)
+      if (!categories[categoryId]) {
+        let category = (await model.categories.getById(categoryId))[0]
+        category.tags = []
+        category.searchedTag = false
+        categories[categoryId] = category
+      }
+
+
+
+      if (req.body.categories && req.body.categories.includes(categoryId)) {
+        categories[categoryId].tags.push(tag)
+      } else if (req.body.tags.includes(tagId)) {
+        categories[categoryId].searchedTag = true
+        categories[categoryId].tags.push(tag)
+      } else if (categories[tag.categoryId] && categories[tag.categoryId].searchedTag) {
+        categories[categoryId].tags.push(tag)
+      }
+    }
+
+    let flatResult = []
+    for (const categoryId in categories) {
+      delete categories[categoryId].searchedTag
+      flatResult.push(categories[categoryId])
+    }
+    res.status(200).send(flatResult)
   } catch (err) {
-    next(err)
+    next(err);
   }
 }
 
@@ -42,6 +85,8 @@ async function searchConversation(req, res, next) {
     } else if (req.body.text) {
       conversation = await model.search.conversation.getByIdsAndText(convTag, req.body.text)
     }
+
+    //TODO: add tag and category listing with conversation
 
     res.status(200).send(conversation)
   } catch (err) {
