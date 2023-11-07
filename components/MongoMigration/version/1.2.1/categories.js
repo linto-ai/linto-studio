ba52ce2489c8ae656ecca44a162f747514958ccd
HOUPERT <yoann.houpert@yahoo.fr>
2023-11-07 15:13:00 +0100

    Added scope to default category

---
+++ components/MongoMigration/version/1.2.1/categories.js
@@ -0,0 +1,17 @@
+const debug = require('debug')(`linto:components:MongoMigration:controllers:version:1.2.1:categories`)
+
+const collections_name = 'categories'
+
+const migration_update = {
+  scope: 'nlp-keyword',
+}
+
+module.exports = {
+  async up(db) {
+    db.collection(collections_name).updateMany({ name: 'keyword' }, { $set: migration_update })
+  },
+
+  async down(db) {
+    db.collection(collections_name).updateMany({ name: 'keyword' }, { $unset: migration_update })
+  }
+}
