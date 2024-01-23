a1ad71adc4fb541dbbc60ae871ee453c65fecaa7
yhoupert <yhoupert@debian.home>
2024-01-23 10:09:14 +0100

    Keyword default category updated with mongo migration

---
+++ studio-api/components/MongoMigration/version/1.4.0/categories.js
@@ -0,0 +1,16 @@
+const debug = require('debug')(`linto:components:MongoMigration:controllers:version:1.2.1:categories`)
+
+const collections_name = 'categories'
+
+const migration_update = {
+  color: 'deep-purple'
+}
+
+module.exports = {
+  async up(db) {
+    db.collection(collections_name).updateMany({ name: 'keyword' }, { $set: migration_update })
+  },
+
+  async down(db) {
+  }
+}
