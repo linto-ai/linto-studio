7951eda4d58d2cd57737a68560c8bcd8bb1055b8
yhoupert <yhoupert@debian.home>
2024-01-30 14:51:38 +0100

    Added tag metadata API (create, update, delete)

---
+++ studio-api/components/WebServer/error/exception/metadata.js
@@ -0,0 +1,47 @@
+/****************
+*****metadata****
+*****************/
+
+const ExceptionType = 'tag'
+
+class MetadataError extends Error {
+  constructor(message, err) {
+    super()
+    this.name = 'MetadataError'
+    this.type = ExceptionType
+    this.status = '400'
+    if (message) this.message = message
+    else this.message = `Metadata error`
+    if (err) this.err = err
+  }
+}
+
+class MetadataNotFound extends Error {
+  constructor(message, err) {
+    super()
+    this.name = 'MetadataNotFound'
+    this.type = ExceptionType
+    this.status = '404'
+    if (message) this.message = message
+    else this.message = `Metadata not found`
+    if (err) this.err = err
+  }
+}
+
+class MetadataUnsupportedMediaType extends Error {
+  constructor(message, err) {
+    super()
+    this.name = 'MetadataUnsupportedMediaType'
+    this.type = ExceptionType
+    this.status = '415'
+    if (message) this.message = message
+    else this.message = `Metadata unsupported media type`
+    if (err) this.err = err
+  }
+}
+
+module.exports = {
+  MetadataError,
+  MetadataNotFound,
+  MetadataUnsupportedMediaType
+}
---
+++ studio-api/components/WebServer/routes/api/taxonomy/metadata.js
@@ -0,0 +1,49 @@
+const debug = require('debug')('linto:conversation-manager:router:api:taxonomy:conversation')
+
+const {
+  getConvMetadata,
+  getTagMetadata,
+  createMetadata,
+  deleteMetadata,
+  updateMetadata
+} = require(`${process.cwd()}/components/WebServer/routecontrollers/taxonomy/metadata.js`)
+
+module.exports = (webserver) => {
+  return [
+    {
+      path: '/conversations/:conversationId/metadata',
+      method: 'get',
+      requireAuth: true,
+      requireReadTaxonomyAccess: true,
+      controller: getConvMetadata
+    },
+    {
+      path: '/conversations/:conversationId/tags/:tagId/metadata',
+      method: 'get',
+      controller: getTagMetadata,
+      requireAuth: true,
+      requireWriteTaxonomyAccess: true
+    },
+    {
+      path: '/conversations/:conversationId/tags/:tagId/metadata',
+      method: 'post',
+      controller: createMetadata,
+      requireAuth: true,
+      requireWriteTaxonomyAccess: true
+    },
+    {
+      path: '/conversations/:conversationId/tags/:tagId/metadata/:metadataId',
+      method: 'delete',
+      controller: deleteMetadata,
+      requireAuth: true,
+      requireWriteTaxonomyAccess: true
+    }, 
+    {
+      path: '/conversations/:conversationId/tags/:tagId/metadata/:metadataId',
+      method: 'patch',
+      controller: updateMetadata,
+      requireAuth: true,
+      requireWriteTaxonomyAccess: true
+    }
+  ]
+}
