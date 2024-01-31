5d00a3d5ed414f12003e0a9fb6bef3717c608650
tjiho <tom.darboux@laposte.net>
2024-01-31 14:36:10 +0100

    Improve css subtitle page

--- studio-frontend/src/components/ScreenActions.vue
+++ studio-frontend/src/components/ScreenActions.vue
@@ -2,16 +2,16 @@
   <div class="turn-actions flex col gap-small justify-center" v-if="canEdit">
     <button
       v-if="leftScreenId && rightScreenId"
-      class="turn-action-btn"
+      class="black"
       title="Merge screens"
       @click="() => $emit('merge', leftScreenId, rightScreenId)">
-      <span class="icon icon-merge"></span>
+      <span class="icon merge"></span>
     </button>
     <button
-      class="turn-action-btn"
+      class="black"
       data-info="Fusionner les tours"
       @click="() => $emit('add', leftScreenId, rightScreenId)">
-      <span class="icon icon-add"></span>
+      <span class="icon plus"></span>
     </button>
   </div>
 </template>
