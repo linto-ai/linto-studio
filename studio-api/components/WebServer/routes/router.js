3dff499d6d92bd068745d566e32336d21178d468
yhoupert <yhoupert@linagora.com>
2024-07-24 16:12:18 +0200

    refactor: transition from internal API to external service with proxy

--- studio-api/components/WebServer/routes/router.js
+++ studio-api/components/WebServer/routes/router.js
@@ -10,106 +10,231 @@ const organization_middlewares = require(
 const taxonomy_middlewares = require(
   `${process.cwd()}/components/WebServer/middlewares/access/taxonomy.js`,
 )
-
 const user_middlewares = require(
   `${process.cwd()}/components/WebServer/middlewares/access/user.js`,
 )
 
+const {
+  createProxyMiddleware,
+  fixRequestBody,
+} = require("http-proxy-middleware")
+
 const ifHasElse = (condition, ifHas, otherwise) => {
   return !condition ? otherwise() : ifHas()
 }
+
+const disableAuthIfDev = (route) => {
+  if (process.env.DEV_DISABLE_AUTH === "true") {
+    route.requireAuth = false
+    route.requireConversationCommentAccess = false
+    route.requireConversationDeleteAccess = false
+    route.requireConversationReadAccess = false
+    route.requireConversationShareAccess = false
+    route.requireConversationWriteAccess = false
+    route.requireDeleteTaxonomyAccess = false
+    route.requireOrganizationAdminAccess = false
+    route.requireOrganizationGuestAccess = false
+    route.requireOrganizationMaintainerAccess = false
+    route.requireOrganizationMemberAccess = false
+    route.requireOrganizationUploaderAccess = false
+    route.requireReadTaxonomyAccess = false
+    route.requireSession = false
+    route.requireUserVisibility = false
+    route.requireWriteTaxonomyAccess = false
+  }
+}
+
+const loadMiddlewares = (route) => {
+  const middlewares = []
+
+  if (route.requireAuth) middlewares.push(auth_middlewares.isAuthenticate)
+  if (route.requireRefresh) middlewares.push(auth_middlewares.refresh_token)
+
+  if (route.requireConversationReadAccess)
+    middlewares.push(conversation_middlewares.asReadAccess)
+  if (route.requireConversationCommentAccess)
+    middlewares.push(conversation_middlewares.asCommentAccess)
+  if (route.requireConversationWriteAccess)
+    middlewares.push(conversation_middlewares.asWriteAccess)
+  if (route.requireConversationDeleteAccess)
+    middlewares.push(conversation_middlewares.asDeleteAccess)
+  if (route.requireConversationShareAccess)
+    middlewares.push(conversation_middlewares.asShareAccess)
+
+  if (route.requireOrganizationAdminAccess)
+    middlewares.push(organization_middlewares.asAdminAccess)
+  if (route.requireOrganizationMaintainerAccess)
+    middlewares.push(organization_middlewares.asMaintainerAccess)
+  if (route.requireOrganizationUploaderAccess)
+    middlewares.push(organization_middlewares.asUploaderAccess)
+  if (route.requireOrganizationMemberAccess)
+    middlewares.push(organization_middlewares.asMemberAccess)
+
+  if (route.requireReadTaxonomyAccess)
+    middlewares.push(taxonomy_middlewares.asReadTaxonomyAccess)
+  if (route.requireWriteTaxonomyAccess)
+    middlewares.push(taxonomy_middlewares.asWriteTaxonomyAccess)
+  if (route.requireDeleteTaxonomyAccess)
+    middlewares.push(taxonomy_middlewares.asDeleteTaxonomyAccess)
+
+  if (route.requireUserVisibility)
+    middlewares.push(user_middlewares.isVisibility)
+
+  if (process.env.LOGGER_ENABLED === "true")
+    middlewares.push(nav_middlewares.logger)
+
+  return middlewares
+}
+
 class Router {
   constructor(webServer) {
     const routes = require("./routes.js")(webServer)
-    for (let level in routes) {
-      for (let path in routes[level]) {
-        const route = routes[level][path]
-        const methods = route.method.split(",")
-        const path_ = route.path.split(",")
 
-        if (process.env.DEV_DISABLE_AUTH === "true") {
-          route.requireAuth = false
-          route.requireConversationCommentAccess = false
-          route.requireConversationDeleteAccess = false
-          route.requireConversationReadAccess = false
-          route.requireConversationShareAccess = false
-          route.requireConversationWriteAccess = false
-          route.requireDeleteTaxonomyAccess = false
-          route.requireOrganizationAdminAccess = false
-          route.requireOrganizationGuestAccess = false
-          route.requireOrganizationMaintainerAccess = false
-          route.requireOrganizationMemberAccess = false
-          route.requireOrganizationUploaderAccess = false
-          route.requireReadTaxonomyAccess = false
-          route.requireSession = false
-          route.requireUserVisibility = false
-          route.requireWriteTaxonomyAccess = false
-        }
+    createApiRoutes(webServer, routes.api_routes)
 
-        //debug('Create route : ' + route.method + ' - ' + level + route.path)
-        let middlewaresLoaded = []
-        // require passport auth (headers)
-        if (route.requireAuth)
-          middlewaresLoaded.push(auth_middlewares.isAuthenticate)
-        if (route.requireRefresh)
-          middlewaresLoaded.push(auth_middlewares.refresh_token)
-
-        // Conversation rights
-        if (route.requireConversationReadAccess)
-          middlewaresLoaded.push(conversation_middlewares.asReadAccess) // require read access
-        if (route.requireConversationCommentAccess)
-          middlewaresLoaded.push(conversation_middlewares.asCommentAccess) // require comment access
-        if (route.requireConversationWriteAccess)
-          middlewaresLoaded.push(conversation_middlewares.asWriteAccess) // require write access
-        if (route.requireConversationDeleteAccess)
-          middlewaresLoaded.push(conversation_middlewares.asDeleteAccess) // require delete access
-        if (route.requireConversationShareAccess)
-          middlewaresLoaded.push(conversation_middlewares.asShareAccess) // require delete access
-
-        // Organization rights
-        if (route.requireOrganizationAdminAccess)
-          middlewaresLoaded.push(organization_middlewares.asAdminAccess)
-        if (route.requireOrganizationMaintainerAccess)
-          middlewaresLoaded.push(organization_middlewares.asMaintainerAccess)
-        if (route.requireOrganizationUploaderAccess)
-          middlewaresLoaded.push(organization_middlewares.asUploaderAccess)
-        if (route.requireOrganizationMemberAccess)
-          middlewaresLoaded.push(organization_middlewares.asMemberAccess)
-
-        // Taxonomy rights
-        if (route.requireReadTaxonomyAccess)
-          middlewaresLoaded.push(taxonomy_middlewares.asReadTaxonomyAccess)
-        if (route.requireWriteTaxonomyAccess)
-          middlewaresLoaded.push(taxonomy_middlewares.asWriteTaxonomyAccess)
-        if (route.requireDeleteTaxonomyAccess)
-          middlewaresLoaded.push(taxonomy_middlewares.asDeleteTaxonomyAccess)
-
-        // User visibility
-        if (route.requireUserVisibility)
-          middlewaresLoaded.push(user_middlewares.isVisibility)
-
-        if (process.env.LOGGER_ENABLED === "true")
-          middlewaresLoaded.push(nav_middlewares.logger)
-
-        methods.map((method) => {
-          path_.map((path) => {
-            webServer.express[method](
-              level + path,
-              middlewaresLoaded,
-              (req, res, next) => {
-                next()
-              },
-              ifHasElse(
-                Array.isArray(route.controller),
-                () => Object.values(route.controller),
-                () => route.controller,
-              ),
-            )
-          })
-        })
-      }
+    if (routes.proxy_routes.length > 0) {
+      createProxyRoutes(webServer, routes.proxy_routes)
     }
   }
 }
 
+const createApiRoutes = (webServer, api_routes) => {
+  for (let level in api_routes) {
+    for (let path in api_routes[level]) {
+      const route = api_routes[level][path]
+
+      const methods = route.method.split(",")
+      const path_ = route.path.split(",")
+
+      disableAuthIfDev(route)
+
+      const middlewares = loadMiddlewares(route)
+
+      methods.map((method) => {
+        path_.map((path) => {
+          webServer.express[method](
+            level + path,
+            middlewares,
+            (req, res, next) => {
+              next()
+            },
+            ifHasElse(
+              Array.isArray(route.controller),
+              () => Object.values(route.controller),
+              () => route.controller,
+            ),
+          )
+        })
+      })
+    }
+  }
+}
+
+const createProxyRoutes = (webServer, proxy_routes) => {
+  proxy_routes.forEach((proxy) => {
+    const serviceHost = proxy.proxyHost
+    const basePath = proxy.basePath
+
+    proxy.proxyPaths.forEach((proxyPath) => {
+      if (proxyPath.disabled) return
+
+      const middlewares = loadMiddlewares(proxyPath)
+
+      proxyPath.paths.forEach((path) => {
+        //we alter req.payload, req.params, req.query, req.body if require
+        path.method.forEach((method) => {
+          const proxy = createProxyMiddleware({
+            target: serviceHost,
+            changeOrigin: true,
+            on: {
+              proxyReq: fixRequestBody,
+            },
+            pathRewrite: (path, req) => {
+              // Removing the linto-studio path from the request
+              let newPath = path.replace(basePath, "")
+              if (proxyPath.scrapPath)
+                return newPath.replace(proxyPath.scrapPath, "")
+              return newPath
+            },
+            onProxyReq: (proxyReq, req, res) => {
+              // If the body is already parsed and present, need to re-write it to the proxy request
+              if (req.body) {
+                const bodyData = JSON.stringify(req.body)
+                proxyReq.setHeader(
+                  "Content-Length",
+                  Buffer.byteLength(bodyData),
+                )
+                proxyReq.setHeader("Content-Type", "application/json")
+                proxyReq.write(bodyData)
+              }
+            },
+            onProxyRes: (proxyRes, req, res) => {
+              let responseData = ""
+              proxyRes.on("data", (chunk) => {
+                responseData += chunk
+              })
+              proxyRes.on("end", () => {
+                res.setHeader("Content-Type", "application/json")
+                res.send(responseData)
+              })
+            },
+            onError: (err, req, res) => {
+              debug("Proxy error:", err)
+              res
+                .status(500)
+                .json({ error: "Proxy error", details: err.message })
+            },
+            logLevel: "debug",
+          })
+
+          debug(`Proxying ${method} ${basePath + path.path} to ${serviceHost}`)
+          webServer.express[method](
+            basePath + path.path,
+            middlewares,
+            (req, res, next) => {
+              addProxyParams(req, path)
+              proxy(req, res, next)
+            },
+          )
+        })
+      })
+    })
+  })
+}
+
+function getNestedValue(obj, path) {
+  return path.split(".").reduce((acc, part) => acc && acc[part], obj)
+}
+
+// Helper function to set nested value
+function setNestedValue(obj, path, value) {
+  const parts = path.split(".")
+  const last = parts.pop()
+  const target = parts.reduce((acc, part) => (acc[part] = acc[part] || {}), obj)
+  target[last] = value
+}
+
+// Middleware function
+function addProxyParams(req, path) {
+  try {
+    if (path.forwardParams !== undefined) {
+      path.forwardParams.forEach((param) => {
+        const [targetPath, sourcePath] = Object.entries(param)[0]
+        const sourceValue = getNestedValue(req, sourcePath)
+        if (sourceValue !== undefined) {
+          setNestedValue(req, targetPath, sourceValue)
+        }
+      })
+    }
+
+    if (path.addParams !== undefined) {
+      path.addParams.forEach((param) => {
+        const [targetPath, value] = Object.entries(param)[0]
+        setNestedValue(req, targetPath, value)
+      })
+    }
+  } catch (err) {
+    debug(err)
+  }
+}
 module.exports = (webServer) => new Router(webServer)
