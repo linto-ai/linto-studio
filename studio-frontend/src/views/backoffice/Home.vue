63610641bb48769ebd2a683f0875ed9572bf66eb
tjiho <tom.darboux@laposte.net>
2024-09-17 14:50:58 +0200

    Add stats to backoffice home

--- studio-frontend/src/views/backoffice/Home.vue
+++ studio-frontend/src/views/backoffice/Home.vue
@@ -1,15 +1,50 @@
 <template>
-  <MainContentBackoffice> todo </MainContentBackoffice>
+  <MainContentBackoffice :loading="loading">
+    <div class="flex gap-medium">
+      <StatCard :count="userCount" title="Users" icon="profile" />
+      <StatCard :count="organizationCount" title="Organizations" icon="work" />
+    </div>
+  </MainContentBackoffice>
 </template>
 <script>
+import { apiGetAllUsers, apiGetAllOrganizations } from "@/api/admin.js"
+
 import MainContentBackoffice from "@/components/MainContentBackoffice.vue"
+import StatCard from "@/components/StatCard.vue"
 export default {
   props: {},
   data() {
-    return {}
+    return {
+      loading: true,
+      users: [],
+      organizationList: [],
+    }
   },
-  mounted() {},
-  methods: {},
-  components: { MainContentBackoffice },
+  mounted() {
+    this.fetchAll()
+  },
+  methods: {
+    async fetchAll() {
+      this.loading = true
+      await this.fetchAllUsers()
+      await this.fetchAllOrganizations()
+      this.loading = false
+    },
+    async fetchAllUsers() {
+      this.users = await apiGetAllUsers()
+    },
+    async fetchAllOrganizations() {
+      this.organizationList = await apiGetAllOrganizations()
+    },
+  },
+  computed: {
+    userCount() {
+      return this.users.length
+    },
+    organizationCount() {
+      return this.organizationList.length
+    },
+  },
+  components: { MainContentBackoffice, StatCard },
 }
 </script>
