fb34e170f15f2410a1c500d2a76b85e316b405e5
tjiho <tom.darboux@laposte.net>
2024-09-30 14:01:23 +0200

    add theme files

---
+++ studio-frontend/themes/elysee/index.js
@@ -0,0 +1,12 @@
+const theme = {
+  stylePath: "themes/elysee/style/style.scss",
+  locales: ["fr-FR", "en-US"],
+  defaultLocale: "fr-FR",
+  localesStrings: {},
+  defaultEnvValues: {
+    VUE_APP_LOGO: "Logo_elysee.png",
+  },
+}
+
+export default theme
+
