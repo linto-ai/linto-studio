4307486f9e0b5ec81d1d7b943cfd60dcff17f3d0
tjiho <tom.darboux@laposte.net>
2024-10-22 18:41:44 +0200

    Add error handling

--- studio-frontend/src/components/QuickSessionCreateContent.vue
+++ studio-frontend/src/components/QuickSessionCreateContent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <form @submit="createQuickSession">
     <section class="flex col gap-small">
       <h2>{{ $t("quick_session.creation.source_title") }}</h2>
       <FormRadio :field="fieldSource" v-model="fieldSource.value" />
@@ -26,14 +26,17 @@
         <span class="label">{{ formSubmitLabel }}</span>
       </button>
     </div>
-  </div>
+  </form>
 </template>
 <script>
 import EMPTY_FIELD from "@/const/emptyField"
 import FormRadio from "@/components/FormRadio.vue"
 import TranscriberProfileSelector from "@/components/TranscriberProfileSelector.vue"
+import { testFieldEmpty } from "@/tools/fields/testEmpty"
+import { formsMixin } from "@/mixins/forms.js"
 
 export default {
+  mixins: [formsMixin],
   props: {
     transcriberProfiles: {
       type: Array,
@@ -42,6 +45,7 @@ export default {
   },
   data() {
     return {
+      fields: ["fieldSource"],
       fieldSource: {
         value: null,
         error: null,
@@ -58,13 +62,36 @@ export default {
             label: this.$i18n.t("quick_session.creation.visio_source_label"),
           },
         ],
+        testField: testFieldEmpty,
       },
       selectedProfile: null,
       formSubmitLabel: "Start",
+
+      formError: null,
+      formState: "idle",
     }
   },
   mounted() {},
-  methods: {},
+  methods: {
+    createQuickSession(event) {
+      event?.preventDefault()
+
+      if (!this.selectedProfile) {
+        this.formError = this.$i18n.t(
+          "quick_session.creation.no_profile_selected_error",
+        )
+        this.formState = "error"
+        return false
+      }
+
+      if (this.testFields()) {
+        console.log("ouiiii")
+      } else {
+        this.formState = "error"
+      }
+      return false
+    },
+  },
   components: { FormRadio, TranscriberProfileSelector },
 }
 </script>
