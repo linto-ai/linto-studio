ee5a1eb6861820e5f532b9ff2c91ec07ea99ebc6
yhoupert <yhoupert@linagora.com>
2025-01-06 20:54:23 +0100

    Fix websocket active jobs on post

--- studio-api/components/WebServer/controllers/llm/index.js
+++ studio-api/components/WebServer/controllers/llm/index.js
@@ -8,7 +8,7 @@ const model = require(`${process.cwd()}/lib/mongodb/models`)
 const WebSocket = require("ws") // Import the WebSocket library
 
 const activeJobs = new Map()
-let socket = null
+let socket
 let isSocketConnected = false
 
 async function generateText(conversation, metadata) {
@@ -126,12 +126,18 @@ async function initWebSocketConnection(convExport) {
   if (currentJobs.length === 0) return // No job to track
   const uniqueJobs = [...new Set(currentJobs)]
 
-  socket = new WebSocket(process.env.LLM_GATEWAY_SERVICES_WS)
+  if (!isSocketConnected) {
+    socket = new WebSocket(process.env.LLM_GATEWAY_SERVICES_WS)
+  }
 
   socket.on("open", () => {
     if (socket.readyState === 1) {
       isSocketConnected = true
       socket.send(JSON.stringify(uniqueJobs))
+
+      if (!activeJobs.has(convExport.jobId)) {
+        activeJobs.set(convExport.jobId, convExport)
+      }
     }
   })
 
@@ -193,7 +199,10 @@ async function processJobWithWebSocket(jobsId, conversationExport) {
       throw new Error("Job ID is required")
     }
     if (activeJobs.has(jobsId)) return
+
     if (jobsId !== null) activeJobs.set(jobsId, conversationExport)
+    else activeJobs.set(conversationExport.jobId, conversationExport)
+
     initWebSocketConnection(conversationExport)
   } catch (err) {
     conversationExport.status = "error"
