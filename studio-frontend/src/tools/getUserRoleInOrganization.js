742f2e059a0b60bb60bef8290bb7abc054ae870d
tjiho <tom.darboux@laposte.net>
2025-02-05 14:44:32 +0100

    Fix: On getUserRoleInOrganization return 0 if organization is not well defined

--- studio-frontend/src/tools/getUserRoleInOrganization.js
+++ studio-frontend/src/tools/getUserRoleInOrganization.js
@@ -1,11 +1,15 @@
 export function getUserRoleInOrganization(organization, userId) {
   if (!organization || !userId) return 0
 
-  if (organization.owner === userId) return 6
-  else {
-    let findUser = organization.users.filter(
-      (usr) => usr._id === userId || usr.userId === userId,
-    )
-    return findUser.length > 0 ? findUser[0].role : 0
+  try {
+    if (organization.owner === userId) return 6
+    else {
+      let findUser = organization.users.filter(
+        (usr) => usr._id === userId || usr.userId === userId,
+      )
+      return findUser.length > 0 ? findUser[0].role : 0
+    }
+  } catch (error) {
+    return 0
   }
 }
