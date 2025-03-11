4b7a799e99fa5e631e6dfc4b4594f7a1251a3e96
tjiho <tom.darboux@laposte.net>
2025-03-11 14:52:53 +0100

    Clean code

--- studio-frontend/src/components/MardownWYSIWYGEditor.vue
+++ studio-frontend/src/components/MardownWYSIWYGEditor.vue
@@ -1,19 +1,8 @@
 <template>
-  <div class="flex wisiwyg" id="markdown-editor-container"></div>
+  <article class="flex wisiwyg" id="markdown-editor-container"></article>
 </template>
 <script>
-import { bus } from "@/main.js"
-
-// import { EditorView } from "prosemirror-view"
-// import { EditorState } from "prosemirror-state"
-// import {
-//   schema,
-//   defaultMarkdownParser,
-//   defaultMarkdownSerializer,
-// } from "prosemirror-markdown"
-
 import { Editor } from "@tiptap/core"
-import StarterKit from "@tiptap/starter-kit"
 import Document from "@tiptap/extension-document"
 import Paragraph from "@tiptap/extension-paragraph"
 import Text from "@tiptap/extension-text"
@@ -31,6 +20,10 @@ export default {
       type: String,
       required: true,
     },
+    readonly: {
+      type: Boolean,
+      default: false,
+    },
   },
   data() {
     return {
@@ -38,23 +31,8 @@ export default {
     }
   },
   mounted() {
-    // const mySchema = new Schema({
-    //   nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
-    //   marks: schema.spec.marks,
-    // })
-    // this.editor = new EditorView(
-    //   document.getElementById("markdown-editor-container"),
-    //   {
-    //     state: EditorState.create({
-    //       doc: defaultMarkdownParser.parse(this.value),
-    //       schema,
-    //     }),
-    //   },
-    // )
     this.editor = new Editor({
-      // bind Tiptap to the `.element`
       element: document.getElementById("markdown-editor-container"),
-      // register extensions
       extensions: [
         Document,
         Paragraph,
@@ -65,11 +43,8 @@ export default {
         TableRow,
         TableHeader,
         TableCell,
-        //Markdown,
         Markdown.configure({
           tightLists: true, // No <p> inside <li> in markdown output
-          //tightListClass: "tight", // Add class to <ul> allowing you to remove <p> margins when tight
-          //bulletListMarker: "* ", // <li> prefix in markdown output
         }),
         ListItem,
         BulletList,
@@ -77,19 +52,22 @@ export default {
           levels: [1, 2, 3, 4],
         }),
       ],
-      // set the initial content
       content: "",
-      // place the cursor in the editor after initialization
       autofocus: true,
-      // make the text editable (default is true)
-      editable: true,
-      // prevent loading the default CSS (which isn't much anyway)
+      editable: !this.readonly,
       injectCSS: false,
     })
-    console.log(this.value)
     this.editor.commands.setContent(this.value)
+    this.editor.on("update", this.onUpdate.bind(this))
   },
-  methods: {},
+  beforeDestroy() {
+    this.editor.destroy()
+  },
+  methods: {
+    onUpdate() {
+      this.$emit("input", this.editor.storage.markdown.getMarkdown())
+    },
+  },
   components: {},
 }
 </script>
