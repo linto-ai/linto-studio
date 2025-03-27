4ccb4bc99f21ef6d85983d46c1b353a0e4d77d1b
tjiho <tom.darboux@laposte.net>
2025-03-27 16:48:55 +0100

    Add templates on transcriber profile editor

--- studio-frontend/src/components/TranscriberProfileEditor.vue
+++ studio-frontend/src/components/TranscriberProfileEditor.vue
@@ -1,15 +1,36 @@
 <template>
-    <div class="flex col transcriber-profile-editor">
-        <Tabs :tabs="tabs" v-model="selectedTab"></Tabs>
-        <TranscriberProfileEditorPlain v-if="selectedTab === 'plain'" v-model="l_transcriberProfile" class="flex1"/>
-        
+  <div class="flex col transcriber-profile-editor">
+    <div class="flex gap-medium align-end">
+      <div class="form-field flex col">
+        <label class="form-label">Type de transcriber:</label>
+        <select v-model="currentType">
+          <option v-for="type in types" :key="type" :value="type">
+            {{ type }}
+          </option>
+        </select>
+      </div>
+
+      <FormCheckbox
+        column
+        switchDisplay
+        v-model="quickMeetingField.value"
+        :field="quickMeetingField" />
+
+      <div></div>
     </div>
+    <TranscriberProfileEditorPlain
+      v-model="l_transcriberProfile"
+      class="flex1" />
+  </div>
 </template>
 <script>
-
 import { bus } from "@/main.js"
+import TRANSCRIBER_PROFILES_TEMPLATES from "@/const/transcriberProfilesTemplates"
+import EMPTY_FIELD from "@/const/emptyField"
 import Tabs from "@/components/Tabs.vue"
 import TranscriberProfileEditorPlain from "@/components/TranscriberProfileEditorPlain.vue"
+import FormCheckbox from "@/components/FormCheckbox.vue"
+
 export default {
   props: {
     transcriberProfile: {
@@ -19,19 +40,17 @@ export default {
   },
   data() {
     return {
-        tabs: [{
-          name: "plain",
-          label: "Plain text",
-        }, {
-          name: "form",
-          label: "Form",
-        }],
-        selectedTab: "plain",
+      types: ["linto", "microsoft"],
+      quickMeetingField: {
+        ...EMPTY_FIELD,
+        value: this.transcriberProfile.quickMeeting,
+        label: this.$t(
+          "backoffice.transcriber_profile_detail.quick_meeting_label",
+        ),
+      },
     }
-    
   },
-  mounted() {
-  },
+  mounted() {},
   computed: {
     l_transcriberProfile: {
       get() {
@@ -41,11 +60,28 @@ export default {
         this.$emit("update:transcriberProfile", value)
       },
     },
+    currentType: {
+      get() {
+        return this.transcriberProfile.config.type
+      },
+      set(value) {
+        this.l_transcriberProfile = TRANSCRIBER_PROFILES_TEMPLATES[value]
+      },
+    },
+  },
+  watch: {
+    "quickMeetingField.value"(value) {
+      this.l_transcriberProfile.quickMeeting = value
+    },
+    "transcriberProfile.quickMeeting"(value) {
+      this.quickMeetingField.value = value
+    },
   },
   methods: {},
   components: {
     Tabs,
-    TranscriberProfileEditorPlain
+    TranscriberProfileEditorPlain,
+    FormCheckbox,
   },
 }
 </script>
