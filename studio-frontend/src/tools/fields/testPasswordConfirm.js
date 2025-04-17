a4ed862572536e95ff654c3904cb202127f2f0e8
tjiho <tom.darboux@laposte.net>
2025-04-17 17:05:07 +0200

    Fix: comparing password field

--- studio-frontend/src/tools/fields/testPasswordConfirm.js
+++ studio-frontend/src/tools/fields/testPasswordConfirm.js
@@ -1,10 +1,9 @@
 export function testPasswordConfirm(field, password, t) {
   field.valid = false
   field.error = null
-  field.value = field.value.trim()
   if (field.value.length === 0) {
     field.error = t("error.required")
-  } else if (field.value !== password) {
+  } else if (field.value !== password?.value) {
     field.error = t("error.passwordsMustBeTheSame")
   } else {
     field.valid = true
