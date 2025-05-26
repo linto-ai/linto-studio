02b0dde528ce7868d08ae7cadd15337e7c1c6a39
tjiho <tom.darboux@laposte.net>
2025-05-26 11:27:41 +0200

    add basic search

--- studio-frontend/src/components/HeaderBar.vue
+++ studio-frontend/src/components/HeaderBar.vue
@@ -1,24 +1,38 @@
 <template>
   <div class="flex align-center header-bar">
-    <input
-      type="search"
-      placeholder="Search"
-      class="search-input header-bar__search" />
-    <!-- <button class="transparent icon-only">
-      <span class="icon filter" />
-    </button> -->
-    <CustomSelect
-      buttonClass="transparent icon-only"
-      v-model="selectedFilters"
-      multipleSelection
-      class="icon-only"
-      :options="filterOptions">
-      <template slot="button-content"><span class="icon filter" /></template>
-    </CustomSelect>
+    <form class="flex row align-center flex1 gap-small" @submit="handleSearch">
+      <div class="flex row align-center">
+        <input
+          v-model="search"
+          type="search"
+          placeholder="Search"
+          class="header-bar__search" />
+        <button type="submit" class="transparent icon-only">
+          <span class="icon search" />
+        </button>
+        <button
+          v-if="search"
+          type="reset"
+          class="transparent icon-only"
+          @click="clearSearch">
+          <span class="icon close" />
+        </button>
+      </div>
+      <!-- <CustomSelect
+        buttonClass="transparent icon-only"
+        v-model="selectedFilters"
+        multipleSelection
+        class="icon-only"
+        :options="filterOptions">
+        <template slot="button-content"><span class="icon filter" /></template>
+      </CustomSelect> -->
+    </form>
   </div>
 </template>
 <script>
 import { bus } from "@/main.js"
+import { mapGetters } from "vuex"
+
 import CustomSelect from "./molecules/CustomSelect.vue"
 export default {
   props: {},
@@ -32,10 +46,33 @@ export default {
         ],
       },
       selectedFilters: [],
+      search: this.$route.query.q || "",
     }
   },
   mounted() {},
-  methods: {},
+  computed: {
+    ...mapGetters("organizations", {
+      currentOrganizationScope: "getCurrentOrganizationScope",
+    }),
+  },
+  methods: {
+    handleSearch(e) {
+      e.preventDefault()
+      this.$router.push({
+        name: "explore",
+        query: { q: this.search },
+        params: { organizationId: this.currentOrganizationScope },
+      })
+      return false
+    },
+    clearSearch() {
+      this.$router.push({
+        name: "explore",
+        query: { q: "" },
+        params: { organizationId: this.currentOrganizationScope },
+      })
+    },
+  },
   components: {
     CustomSelect,
   },
