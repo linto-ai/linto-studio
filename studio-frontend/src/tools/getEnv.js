b1f7aa6a9268ec5962e3b0c57f3b781ad056bd0e
tjiho <tom.darboux@laposte.net>
2025-06-11 11:49:13 +0200

    Add new logo

--- studio-frontend/src/tools/getEnv.js
+++ studio-frontend/src/tools/getEnv.js
@@ -3,5 +3,5 @@ import getCurrentTheme from "./getCurrentTheme"
 
 export function getEnv(envKey) {
   const themeEnv = getCurrentTheme()?.defaultEnvValues || {}
-  return themeEnv[envKey] || process.env[envKey] || DEFAULTENV[envKey]
+  return process.env[envKey] || themeEnv[envKey] || DEFAULTENV[envKey]
 }
