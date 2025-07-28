eb4e6c479c29f1008d8108c90bc7fed34ec3678f
tjiho <tom.darboux@laposte.net>
2025-07-28 11:46:16 +0200

    Front: get token from query params in oidc

--- studio-frontend/src/views/oidc.vue
+++ studio-frontend/src/views/oidc.vue
@@ -3,7 +3,7 @@
 </template>
 <script>
 import Loading from "@/components/atoms/Loading.vue"
-import { getOidcToken } from "@/api/user.js"
+import { getOidcToken, apiGetPersonalUserInfo } from "@/api/user.js"
 import { setCookie } from "@/tools/setCookie.js"
 
 export default {
@@ -16,22 +16,34 @@ export default {
   },
   methods: {
     async fetchToken() {
-      let res = await getOidcToken()
-
-      if (res.status == "error") {
-        this.$router.replace({ name: "login", query: { error: "oidc" } })
-      }
-
-      if (res.status == "success") {
-        setCookie("userId", res.data.user_id, 7)
-        setCookie("authToken", res.data.auth_token, 7)
+      if (this.$route?.query?.token) {
+        setCookie("authToken", this.$route?.query?.token, 7)
         setCookie("cm_orga_scope", "")
 
-        if (this.$route?.query?.next) {
-          window.location.href = this.$route?.query?.next
-        } else {
-          window.location.href = "/"
+        const userReq = await apiGetPersonalUserInfo()
+        if (userReq.status == "success") {
+          setCookie("userId", userReq.data._id, 7)
+          this.redirect()
+          return
         }
+      } else {
+        let res = await getOidcToken()
+
+        if (res.status == "success") {
+          setCookie("userId", res.data.user_id, 7)
+          setCookie("authToken", res.data.auth_token, 7)
+          setCookie("cm_orga_scope", "")
+          this.redirect()
+          return
+        }
+      }
+      this.$router.replace({ name: "login", query: { error: "oidc" } })
+    },
+    async redirect() {
+      if (this.$route?.query?.next) {
+        window.location.href = this.$route?.query?.next
+      } else {
+        window.location.href = "/"
       }
     },
   },
