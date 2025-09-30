bdfa83af2870c6ea5434579e565825ebb795d05d
tjiho <tom.darboux@laposte.net>
2025-09-30 13:21:12 +0200

    Set test file as env variable

--- studio-sdk/javascript/test.js
+++ studio-sdk/javascript/test.js
@@ -2,11 +2,13 @@ import studioSDK from "./index.js"
 import fs from "fs"
 
 const token = process.env.STUDIO_TOKEN
+const filePath = process.env.FILE_PATH
+
 let linto = new studioSDK({
   token,
   baseUrl: "http://127.0.0.1:8001",
   apiPath: "api",
 })
 
-const file = fs.openAsBlob("/home/tom/Musique/audio/baudelaire 2.wav")
+const file = fs.openAsBlob(filePath)
 linto.transcribe(file)
