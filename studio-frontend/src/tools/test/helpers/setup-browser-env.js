db6d39c763b1856d97072ec4cc9f928195c4bdac
tjiho <tom.darboux@laposte.net>
2025-11-12 17:02:11 +0100

    wip

--- studio-frontend/src/tools/test/helpers/setup-browser-env.js
+++ studio-frontend/src/tools/test/helpers/setup-browser-env.js
@@ -1,3 +1,10 @@
-import browserEnv from "browser-env"
+// import browserEnv from "browser-env"
 
-browserEnv()
+// browserEnv()
+
+import { JSDOM } from "jsdom"
+
+const dom = new JSDOM(`<!DOCTYPE html><html><body></body></html>`)
+
+global.window = dom.window
+global.document = dom.window.document
