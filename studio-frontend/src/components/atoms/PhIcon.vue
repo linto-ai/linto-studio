48ba6536cc19426ff746cd5592b6b68b55d86fd7
tjiho <tom.darboux@laposte.net>
2026-02-05 16:18:38 +0100

    Optimise icon load

--- studio-frontend/src/components/atoms/PhIcon.vue
+++ studio-frontend/src/components/atoms/PhIcon.vue
@@ -1,239 +1,49 @@
 <template>
-  <component
-    v-if="iconComponent"
-    :is="iconComponent"
-    :weight="weight"
-    :mirrored="mirrored"
-    :class="['icon-svg', size, color, animation]"
-    :style="{ color: computedColor, ...style }"
-  />
-  <span v-else class="icon-svg missing-icon" :class="[size, color]">?</span>
+  <Icon
+    v-if="iconName"
+    :icon="iconName"
+    :class="['icon-svg', sizeClass, color, animation]"
+    :style="{ color: computedColor, ...sizeStyle }"
+    :horizontal-flip="mirrored" />
+  <span v-else class="icon-svg missing-icon" :class="[sizeClass, color]">?</span>
 </template>
 
 <script>
-import {
-  PhAppleLogo,
-  PhArrowClockwise,
-  PhArrowLeft,
-  PhArrowLineDown,
-  PhArrowLineLeft,
-  PhArrowLineUp,
-  PhArrowRight,
-  PhArrowSquareUp,
-  PhArrowsClockwise,
-  PhBell,
-  PhBracketsCurly,
-  PhBroadcast,
-  PhBuildings,
-  PhCaretDown,
-  PhCaretDoubleLeft,
-  PhCaretUp,
-  PhChartPie,
-  PhCheck,
-  PhCheckCircle,
-  PhCircle,
-  PhCircleNotch,
-  PhClipboard,
-  PhClock,
-  PhClosedCaptioning,
-  PhCopy,
-  PhCreditCard,
-  PhDotsThree,
-  PhDownload,
-  PhDownloadSimple,
-  PhEquals,
-  PhExport,
-  PhEye,
-  PhEyeSlash,
-  PhFile,
-  PhFileAudio,
-  PhFileCsv,
-  PhFileText,
-  PhFileVideo,
-  PhFileXls,
-  PhFloppyDisk,
-  PhGear,
-  PhGlobeHemisphereWest,
-  PhGraduationCap,
-  PhHouse,
-  PhInfo,
-  PhKey,
-  PhLink,
-  PhList,
-  PhMagnifyingGlass,
-  PhMegaphone,
-  PhMicrophone,
-  PhMinusCircle,
-  PhNotebook,
-  PhPaperPlaneTilt,
-  PhPause,
-  PhPencil,
-  PhPencilSimple,
-  PhPlay,
-  PhPlugsConnected,
-  PhPlus,
-  PhPlusCircle,
-  PhPushPin,
-  PhPushPinSlash,
-  PhRecord,
-  PhShareNetwork,
-  PhShieldCheck,
-  PhShieldSlash,
-  PhShieldWarning,
-  PhSignOut,
-  PhSkipBack,
-  PhSkipForward,
-  PhSmileyBlank,
-  PhSparkle,
-  PhSpinner,
-  PhSpinnerGap,
-  PhStar,
-  PhStop,
-  PhSwap,
-  PhTag,
-  PhTranslate,
-  PhTrash,
-  PhTray,
-  PhUpload,
-  PhUser,
-  PhUserSwitch,
-  PhUsers,
-  PhWarning,
-  PhWarningCircle,
-  PhWaves,
-  PhWebcam,
-  PhWrench,
-  PhX,
-  PhXCircle,
-} from 'phosphor-vue';
-
-const ICON_MAP = {
-  AppleLogo: PhAppleLogo,
-  ArrowClockwise: PhArrowClockwise,
-  ArrowLeft: PhArrowLeft,
-  ArrowLineDown: PhArrowLineDown,
-  ArrowLineLeft: PhArrowLineLeft,
-  ArrowLineUp: PhArrowLineUp,
-  ArrowRight: PhArrowRight,
-  ArrowSquareUp: PhArrowSquareUp,
-  ArrowsClockwise: PhArrowsClockwise,
-  Bell: PhBell,
-  BracketsCurly: PhBracketsCurly,
-  Broadcast: PhBroadcast,
-  Buildings: PhBuildings,
-  CaretDown: PhCaretDown,
-  CaretDoubleLeft: PhCaretDoubleLeft,
-  CaretUp: PhCaretUp,
-  ChartPie: PhChartPie,
-  Check: PhCheck,
-  CheckCircle: PhCheckCircle,
-  Circle: PhCircle,
-  CircleNotch: PhCircleNotch,
-  Clipboard: PhClipboard,
-  Clock: PhClock,
-  ClosedCaptioning: PhClosedCaptioning,
-  Copy: PhCopy,
-  CreditCard: PhCreditCard,
-  DotsThree: PhDotsThree,
-  Download: PhDownload,
-  DownloadSimple: PhDownloadSimple,
-  Equals: PhEquals,
-  Export: PhExport,
-  Eye: PhEye,
-  EyeSlash: PhEyeSlash,
-  File: PhFile,
-  FileAudio: PhFileAudio,
-  FileCsv: PhFileCsv,
-  FileText: PhFileText,
-  FileVideo: PhFileVideo,
-  FileXls: PhFileXls,
-  FloppyDisk: PhFloppyDisk,
-  Gear: PhGear,
-  GlobeHemisphereWest: PhGlobeHemisphereWest,
-  GraduationCap: PhGraduationCap,
-  House: PhHouse,
-  Info: PhInfo,
-  Key: PhKey,
-  Link: PhLink,
-  List: PhList,
-  MagnifyingGlass: PhMagnifyingGlass,
-  Megaphone: PhMegaphone,
-  Microphone: PhMicrophone,
-  MinusCircle: PhMinusCircle,
-  Notebook: PhNotebook,
-  PaperPlaneTilt: PhPaperPlaneTilt,
-  Pause: PhPause,
-  Pencil: PhPencil,
-  PencilSimple: PhPencilSimple,
-  Play: PhPlay,
-  PlugsConnected: PhPlugsConnected,
-  Plus: PhPlus,
-  PlusCircle: PhPlusCircle,
-  PushPin: PhPushPin,
-  PushPinSlash: PhPushPinSlash,
-  Record: PhRecord,
-  ShareNetwork: PhShareNetwork,
-  ShieldCheck: PhShieldCheck,
-  ShieldSlash: PhShieldSlash,
-  ShieldWarning: PhShieldWarning,
-  SignOut: PhSignOut,
-  SkipBack: PhSkipBack,
-  SkipForward: PhSkipForward,
-  SmileyBlank: PhSmileyBlank,
-  Sparkle: PhSparkle,
-  Spinner: PhSpinner,
-  SpinnerGap: PhSpinnerGap,
-  Star: PhStar,
-  Stop: PhStop,
-  Swap: PhSwap,
-  Tag: PhTag,
-  Translate: PhTranslate,
-  Trash: PhTrash,
-  Tray: PhTray,
-  Upload: PhUpload,
-  User: PhUser,
-  UserSwitch: PhUserSwitch,
-  Users: PhUsers,
-  Warning: PhWarning,
-  WarningCircle: PhWarningCircle,
-  Waves: PhWaves,
-  Webcam: PhWebcam,
-  Wrench: PhWrench,
-  X: PhX,
-  XCircle: PhXCircle,
-};
+import { Icon } from "@iconify/vue2"
 
 export default {
-  name: 'PhIcon',
+  name: "PhIcon",
+  components: { Icon },
   props: {
-    name: { type: String, required: true }, // ex: 'house', 'user', 'arrow-left'
-    size: { type: [String, Number], default: 'sm' }, // xs, sm, md, lg, xl
-    color: { type: String, default: '' }, // primary, secondary, etc.
-    weight: { type: String, default: 'fill' }, // thin, light, regular, bold, fill, duotone
+    name: { type: String, required: true },
+    size: { type: [String, Number], default: "sm" },
+    color: { type: String, default: "" },
+    weight: { type: String, default: "fill" },
     mirrored: { type: Boolean, default: false },
-    animation: { type: String, default: '' } // spin, pulse
+    animation: { type: String, default: "" },
   },
   computed: {
-    iconComponent() {
-      const camelCase = this.name.replace(/-([a-z])/g, (_, letter) => letter.toUpperCase());
-      const pascalCase = camelCase.charAt(0).toUpperCase() + camelCase.slice(1);
-      return ICON_MAP[pascalCase] || null;
+    iconName() {
+      if (!this.name) return null
+      const w = this.weight
+      const suffix = w && w !== "regular" ? "-" + w : ""
+      return `ph:${this.name}${suffix}`
     },
-    style() {
-      const size = Number(this.size)
-      if (size) {
-        return {
-          width: size + 'px',
-          height: size + 'px',
-        }
+    sizeClass() {
+      return isNaN(this.size) ? this.size : null
+    },
+    sizeStyle() {
+      const px = Number(this.size)
+      if (px) {
+        return { width: px + "px", height: px + "px" }
       }
       return {}
     },
     computedColor() {
-      return this.color || 'currentColor';
-    }
-  }
-};
+      return this.color || "currentColor"
+    },
+  },
+}
 </script>
 
 <style lang="scss" scoped>
@@ -319,4 +129,4 @@ export default {
   font-size: 1.2em;
   opacity: 0.5;
 }
-</style> 
+</style>
--- studio-frontend/vite.config.js
+++ studio-frontend/vite.config.js
@@ -1,14 +1,10 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue2'
 import path from 'path'
 import { fileURLToPath } from 'url'
-
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), 'VUE_APP_')
-
-  return {
+export default defineConfig({
     plugins: [vue()],
 
     resolve: {
@@ -73,5 +69,4 @@ export default defineConfig(({ mode }) => {
         'uuid',
       ],
     },
-  }
 })
