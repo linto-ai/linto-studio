aef22f8228ce811131515758823072ea65f7a286
yhoupert <yhoupert@linagora.com>
2026-05-06 14:35:10 +0200

    Carry session template name into resulting conversation

--- studio-api/components/WebServer/controllers/session/conversation.js
+++ studio-api/components/WebServer/controllers/session/conversation.js
@@ -57,6 +57,7 @@ function initConversationMultiChannel(
         channel_start_time: session.startTime,
         channel_end_time: session.endTime,
       },
+      ...(session.meta?.template ? { template: session.meta.template } : {}),
     },
     jobs: {
       transcription: { state: "done" },
@@ -247,6 +248,7 @@ function initializeCaption(
         channel_end_time: session.endTime,
       },
       normalize: { filter: {} },
+      ...(session.meta?.template ? { template: session.meta.template } : {}),
     },
     sharedWithUsers: [],
     description: "",
